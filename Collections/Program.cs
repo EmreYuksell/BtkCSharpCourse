a2b7fbd5e3e3498439cdf682f943220ea8b89f52
Emre YÜKSEL <equesse78@gmail.com>
2023-08-22 17:42:55 +0300

    a

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -10,17 +10,82 @@ namespace Collections
     {
         static void Main(string[] args)
         {
-
+            //ArrayList();
+            //List();
+            
+            Console.ReadLine();
+        }
+        
+        
+        private static void ArrayList()
+        {
             ArrayList cities = new ArrayList();
             cities.Add("Ankara");
             cities.Add("Karabük");
 
-            foreach (var city in cities )
+            foreach (var city in cities)
             {
                 Console.WriteLine(city);
 
             }
-            Console.ReadLine();
+            
         }
+
+
+
+        class Customer
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+
+        }
+        
+        private static void List()
+        {
+            List<string> cities = new List<string>();
+            cities.Add("Ankara");
+            cities.Add("AAA");
+
+            foreach (string city in cities)
+            {
+                Console.WriteLine(city);
+            }
+            //List<Customer> customers = new List<Customer>();
+            //customers.Add(new Customer {Id= 1,FirstName="Emre" });
+            //customers.Add(new Customer { Id = 2, FirstName = "Yüksel" });
+
+            List<Customer> customers = new List<Customer>
+            {
+           new Customer { Id = 1, FirstName = "Emre" },//Farklı yazım
+           new Customer { Id = 2, FirstName = "Yüksel" }
+            };
+
+
+            var customer2 = new Customer
+            {
+                Id = 3,
+                FirstName = "Serkan"
+            };
+
+            customers.Add(customer2);
+            customers.AddRange(new Customer[2]
+            {
+            new Customer{Id=4,FirstName="Ali"},
+            new Customer{Id=5,FirstName="Mehmet"}
+            });
+
+
+
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer.FirstName, customer.Id);
+                Console.WriteLine(customer.Id);
+            }
+            var count = customers.Count;
+            Console.WriteLine("Count: {0}", count);
+        }
+
     }
+    
+
 }
